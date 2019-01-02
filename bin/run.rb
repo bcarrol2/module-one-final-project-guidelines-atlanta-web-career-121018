70a21990f8c106fd18ef700400546c5bd5450d65
github username <github email address>
2019-01-02 10:49:54 -0500

    git comments

--- bin/run.rb
+++ bin/run.rb
@@ -4,3 +4,13 @@ require_relative '../config/environment'
 
 puts "HELLO WORLD"
 puts "Ryans Test"
+
+# TO PUSH
+# git add .
+# git commit -m "Comment"
+# git branch -f origin HEAD
+# git push -f origin master
+
+
+# TO PULL
+# git pull
