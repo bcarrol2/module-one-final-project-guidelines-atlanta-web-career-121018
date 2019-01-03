bdbfa3bdbcd7ae778aa04ecc2ed75c3e5a447be1
Blair <blairacarroll@gmail.com>
2019-01-03 15:21:47 -0500

    deleted methods

--- lib/car.rb
+++ lib/car.rb
@@ -6,11 +6,6 @@ class Car < ActiveRecord::Base
     has_many :users, through: :reviews
 
     # write methods here
-    def top_rated
-      b = Review.all.map do |rev|
-        rev.rating
-      end
-      b.sort {|a, b| b <=> a }.take(5).pop()
-    end
+  
 
 end
--- lib/review.rb
+++ lib/review.rb
@@ -7,18 +7,4 @@ class Review < ActiveRecord::Base
 
     # write methods here
 
-    def review_by_car_id
-      a = Review.all.map {|car| car.car_id}
-      a.take(100)
-    end
-
-    def review_by_author
-      Review.all.map do |rev|
-        rev.author
-      end
-    end
-
-    
-
-
 end
